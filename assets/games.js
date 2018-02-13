ee7c65e53a880f2504a1e0cc08100bafa0a71fae
CNCodes <colindavid.nelson@gmail.com>
2018-02-13 16:06:16 -0700

    did most of the game

---
+++ assets/games.js
@@ -0,0 +1,88 @@
+
+var itsfightTime = false;
+var chosenYourCharacter = false;
+var opponentNotChosen = true;
+var playerChosen;
+var playerAttack;
+var playerHealth;
+var opponentChosen;
+var opponentHealth;
+var opponentAttack;
+var opponentCount = 0;
+$(document).ready(function(){
+
+$(".picture").on("click", function(){
+
+	if(chosenYourCharacter === false){
+
+	playerHealth = parseInt($(this).attr("data-health"));
+	playerAttack = parseInt($(this).attr("data-attack"));
+
+	$(this).removeClass("picture");
+
+	$(this).appendTo("#characterChosen");
+
+	$(".picture").addClass("opponent");
+
+	$(".opponent").appendTo("#enemyPlayers");
+
+	$(".opponent").removeClass("picture");
+
+	console.log(playerHealth, playerAttack);
+
+	chosenYourCharacter = true;
+
+}
+
+});
+
+$(document).on("click", ".opponent", function(){
+
+	if(opponentNotChosen){
+
+		opponentHealth = parseInt($(this).attr("data-health"));
+		opponentAttack = parseInt($(this).attr("data-attack"));
+
+
+		$(this).addClass("defeatMe");
+		$(this).appendTo("#enemyChosen");
+
+		console.log(opponentHealth, opponentAttack);
+
+		opponentNotChosen = false;
+		itsfightTime = true;
+	}
+});
+
+$("#fightButton").on("click", function(){
+	if(itsfightTime){
+
+		playerHealth = playerHealth - opponentAttack;
+
+		opponentHealth = opponentHealth - playerAttack;
+
+		playerAttack = playerAttack + (playerAttack*.5);
+
+		alert(playerHealth);
+		alert(opponentHealth);
+		alert(playerAttack);
+	}
+
+	if(opponentHealth <= 0){
+		alert("You've defeated opponent");
+		$(".defeatMe").appendTo(defeatedCharacter);
+		opponentNotChosen = true;
+		itsfightTime = false;
+
+	}
+
+	if(playerHealth <= 0){
+		alert("You lost");
+		location.reload();
+	}
+})
+
+});
+
+
+
